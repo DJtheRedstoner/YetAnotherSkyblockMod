ac2e8560c39c867743e5427fa6ddb1024d9e222f
Debuggingss <55459283+Debuggingss@users.noreply.github.com>
2020-11-24 00:00:18 +0100

    Update YetAnotherSkyblockMod.java

--- src/main/java/me/djtheredstoner/yetanotherskyblockmod/YetAnotherSkyblockMod.java
+++ src/main/java/me/djtheredstoner/yetanotherskyblockmod/YetAnotherSkyblockMod.java
@@ -19,7 +19,7 @@ import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 import java.util.Set;
 
-@Mod(modid = "yetanotherskyblockmod", name = "YetAnotherSkyblockMod", version = "1.2", acceptedMinecraftVersions = "[1.8.9]", clientSideOnly = true)
+@Mod(modid = "yetanotherskyblockmod", name = "YetAnotherSkyblockMod", version = "1.2", acceptedMinecraftVersions = "[1.16.4,1.16.3,1.16.2,1.16.1,1.15.2,1.15.1,1.15,1.14.4,1.14.3,1.14.2,1.13.2,1.12.2,1.12.1,1.12,1.11.2,1.11,1.10.2,1.10,1.9.4,1.9,1.8.9,1.8.8,1.8,1.7.10_pre4,1.7.10,1.7.2,1.6.4,1.6.3,1.6.2,1.6.1,1.5.2,1.5.1,1.5,1.4.7,1.4.6,1.4.5,1.4.4,1.4.3,1.4.2,1.4.1,1.4.0,1.3.2,1.2.5,1.2.4,1.2.3,1.1]", clientSideOnly = true)
 public class YetAnotherSkyblockMod {
 
     @Mod.Instance
